42524aff1ae407e89bd85140f5b19109e505c834
thisisrow <prathameshmishra2020@gmail.com>
2025-08-10 23:19:37 +0530

    render

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,9 +14,9 @@ app.use(cors({ origin: process.env.ORIGIN || "http://localhost:5173" }));
 
 const server = http.createServer(app);
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017";
-const DB_NAME   = process.env.DB_NAME || "whatsapp";
-const PORT      = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI ;
+const DB_NAME   = process.env.DB_NAME ;
+const PORT      = process.env.PORT ;
 
 let db;
 
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-
-export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+export const API_URL =  "https://whatsapp-webhook-processor-o18g.onrender.com";
 
 export const api = axios.create({
   baseURL: API_URL,
